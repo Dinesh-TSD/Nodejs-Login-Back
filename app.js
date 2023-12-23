d28868a39e2b967ca4ac9b7cc6febe4044429453
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2023-12-23 21:59:55 +0530

    url

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require("express")
-const app = express()
-const products=require("./routes/product")
-const auth=require("./routes/auth")
-const cookieParser=require("cookie-parser")
-const errorMiddleware= require("./middlewares/error")
+const express = require("express");
+const app = express();
+const products = require("./routes/product");
+const auth = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middlewares/error");
+const cors = reqiure("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://dinesh-react-login-front.vercel.app",
+  })
+);
 
-
-app.use("/api/v1/",products)
-app.use("/api/v1/",auth)
+app.use("/api/v1/", products);
+app.use("/api/v1/", auth);
 
 app.use(errorMiddleware);
 
