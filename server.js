29dbe171c30601d73d4efe412919f0cf0c1a4904
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2023-12-23 19:25:04 +0530

    env updated

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ dotenv.config({ path: path.join(__dirname, "config/config.env") })
  
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT} in ${process.env.NODE_ENV}`);
+app.listen(process.env.SMP_PORT, () => {
+    console.log(`server running on port ${process.env.SMP_PORT} in ${process.env.NODE_ENV}`);
 })
