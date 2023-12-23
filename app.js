a7f092d15bfdaf02b2f124ad26a95a2e06f319bb
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2023-12-23 22:03:15 +0530

    url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const products = require("./routes/product");
 const auth = require("./routes/auth");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middlewares/error");
-const cors = reqiure("cors");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
